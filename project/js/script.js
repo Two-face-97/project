aa482c2344c50011e08373bac32aef30e70bebfc
Atom <example@gmail.com>
2020-10-09 21:28:45 +0300

    first commit

---
+++ project/js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+//console.log('arr' + "- object");
+//console.log(4 + +"5");
+
+//let incr = 10,
+//    decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2+2*2 === 8);
+
+//&&
+//||
+
+
+//const isCheced = false,
+//      iclose = false;
+
+//console.log(isCheced || !iclose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user ="Stefan";s
+
+//alert (`Привет, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('hello')
+
+//const result = confirm('u here?');
+//console.log(result);
+
+//const answer = +prompt("сколько вам лет", "");
+//console.log(answer + 5);
+
+//const answer = [];
+
+//answer[0] = prompt('Как вас зовут?', '');
+//answer[1] = prompt('Ваша фамилия?', '');
+//answer[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answer));
+//console.log(typeof(null));
+
