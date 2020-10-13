ea634d3d65812d1148295930ecf8a830b3dfcff9
Atom <example@gmail.com>
2020-10-13 08:56:45 +0300

    first practics

--- project/js/script.js
+++ project/js/script.js
@@ -1,101 +1,21 @@
 "use strict";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//console.log('arr' + "- object");
-//console.log(4 + +"5");
-
-//let incr = 10,
-//    decr = 10;
-
-//incr++;
-//decr--;
-
-//console.log(++incr);
-//console.log(--decr);
-
-//console.log(5%2);
-
-//console.log(2+2*2 === 8);
-
-//&&
-//||
-
-
-//const isCheced = false,
-//      iclose = false;
-
-//console.log(isCheced || !iclose);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const category = 'toys';
-
-//console.log(`https://someurl.com/${category}/5`);
-
-//const user ="Stefan";s
-
-//alert (`Привет, ${user}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//alert('hello')
-
-//const result = confirm('u here?');
-//console.log(result);
-
-//const answer = +prompt("сколько вам лет", "");
-//console.log(answer + 5);
-
-//const answer = [];
-
-//answer[0] = prompt('Как вас зовут?', '');
-//answer[1] = prompt('Ваша фамилия?', '');
-//answer[2] = prompt('Сколько вам лет?', '');
-
-//console.log(typeof(answer));
-//console.log(typeof(null));
-
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    ganres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
